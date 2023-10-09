69257385040c5fb453bb2ea2e4f9b6969d26ade1
xinxu <azzlzzxz@gmail.com>
2023-10-09 19:12:38 +0800

    feat: provider

--- 5-project-provider/src/app.controller.ts
+++ 5-project-provider/src/app.controller.ts
@@ -1,12 +1,41 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  @Inject(AppService) private readonly appService1: AppService;
+  @Inject('app_service') private readonly appService: AppService;
+  @Inject('person') private readonly person: { name: string; age: number };
+  @Inject('person2') private readonly person2: { name: string; age: number };
+  @Inject('person3') private readonly person3: {
+    name: string;
+    desc: string;
+  };
+  @Inject('person4') private readonly person4: {
+    name: string;
+    desc: string;
+  };
+  @Inject('person5') private readonly person5: {
+    name: string;
+    age: number;
+  };
+
+  // constructor(
+  //   @Inject('app_service') private readonly appService: AppService,
+  //   @Inject('person') private readonly person: { name: string; age: number },
+  //   @Inject('person2') private readonly person2: { name: string; age: number },
+  //   @Inject('person3')
+  //   private readonly person3: { name: string; age: number; desc: string },
+  // ) {}
 
   @Get()
   getHello(): string {
+    console.log(this.appService1);
+    console.log(this.person);
+    console.log(this.person2);
+    console.log(this.person3);
+    console.log(this.person4);
+    console.log(this.person5);
     return this.appService.getHello();
   }
 }
--- 5-project-provider/src/app.module.ts
+++ 5-project-provider/src/app.module.ts
@@ -5,6 +5,54 @@ import { AppService } from './app.service';
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'app_service',
+      useClass: AppService,
+    },
+    {
+      provide: 'person',
+      useValue: {
+        name: 'John',
+        age: 25,
+      },
+    },
+    {
+      provide: 'person2',
+      useFactory: () => {
+        return {
+          name: 'alen',
+          age: 15,
+        };
+      },
+    },
+    {
+      provide: 'person3',
+      useFactory: (person: { name: string }, appService: AppService) => {
+        return {
+          name: person.name,
+          desc: appService.getHello(),
+        };
+      },
+      inject: ['person', 'app_service'],
+    },
+    {
+      provide: 'person4',
+      async useFactory() {
+        await new Promise((resolve) => {
+          setTimeout(resolve, 3000);
+        });
+        return {
+          name: 'aaa',
+          desc: 'ccc',
+        };
+      },
+    },
+    {
+      provide: 'person5',
+      useExisting: 'person2',
+    },
+  ],
 })
 export class AppModule {}
